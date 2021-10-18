37cf0d778a6777055f9a922818c85188fe8cbd82
priscilla <pdjarbeng@outlook.com>
2021-10-18 13:45:28 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.getElementById('main').remove('main')
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+
+//newHeader.innerHTML = "Priscilla is the champion!"
+newHeader.textContent = "Priscilla is the champion!"
